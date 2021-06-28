7c082cc07203ac8fcb8ea43d51ae395dbb16dd84
Rodrigo Ramirez <rodrigo8rb@gmail.com>
2021-06-28 15:07:29 -0500

    ✨ BMI Calculator
    ✨ BMI Calculator

---
+++ bmi_calculator/lib/constants/app_constants.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
+
+final Color mainHexColor = HexColor("#1c1c1c");
+final Color accentHexColor = HexColor("#fcc91c");
