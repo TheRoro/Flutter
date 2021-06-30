561b37ebf8b5010868dc519e944a03e71adfcc43
Rodrigo Ramirez <rodrigo8rb@gmail.com>
2021-06-30 11:30:16 -0500

    ✨Started E-Shop project
    ✨Started E-Shop project

---
+++ e_shop/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:e_shop/constants.dart';
+import 'package:e_shop/screens/home/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+          textTheme: Theme.of(context).textTheme.apply(bodyColor: kTextColor),
+          visualDensity: VisualDensity.adaptivePlatformDensity),
+      home: HomeScreen(),
+    );
+  }
+}
